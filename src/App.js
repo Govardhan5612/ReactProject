55bf19b356d7156f1a390150be545d8dc99dd028
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-06-26 22:07:37 +0530

    Display the Bridgelabz logo use Property binding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Image from './logo1.png';
 
 function App() {
   const name = "Bridgelabz";
-  let object ={
-    firstName:"Govardhan",
-    lastName:"Reddy"
+  let object = {
+    firstName: "Govardhan",
+    lastName: "Reddy"
   };
   return (
     <div className="App">
       <h1>Hello From {name}</h1>
+      <img src={Image} alt='' width='300' height='300'></img>
       <h2>First Name : {object.firstName}</h2>
       <h2>Last Name : {object.lastName}</h2>
     </div>
