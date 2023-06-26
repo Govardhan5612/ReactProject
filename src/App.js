d51b58dd2a206803a266142b875895ada7376431
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-06-26 21:37:45 +0530

    Ability to use One Way Data Binding

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Bridgelabz";
+  let object ={
+    firstName:"Govardhan",
+    lastName:"Reddy"
+  };
   return (
     <div className="App">
-      <h>Hello From BridgeLabz</h>
+      <h1>Hello From {name}</h1>
+      <h2>First Name : {object.firstName}</h2>
+      <h2>Last Name : {object.lastName}</h2>
     </div>
   );
 }
